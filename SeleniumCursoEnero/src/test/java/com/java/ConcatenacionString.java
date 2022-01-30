604539a5623829180974ba81e664519ed3c780cb
pablo <pablo@DESKTOP-DLCGN6D>
2022-01-30 09:45:38 -0600

    Ejercicios de Java Clase: 29/02/2022

---
+++ SeleniumCursoEnero/src/test/java/com/java/ConcatenacionString.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class ConcatenacionString {
+
+	public static void main(String[] args) {
+		String Var1, Var2, Var3, Var4;
+		Var1 = "Cool";
+		Var2 = "Selenium con Java";
+		Var3 = "Curso de";
+		Var4 = "Testers";
+
+		System.out.println("" + Var3 + " " + Var2 + " " + Var1 + "" + Var4);
+
+	}
+
+}
---
+++ SeleniumCursoEnero/src/test/java/com/java/OperacionesBasicas.java
@@ -0,0 +1,22 @@
+package com.java;
+
+public class OperacionesBasicas {
+
+	public static void main(String[] args) {
+		double a = 5.6;
+		double b = 7.5;
+		double c = 14.55;
+
+		double d = a + b + c;
+		double e = a * b * c;
+		double f = a / b / c;
+
+		System.out.println("La suma del valor " + a + " mas el valor " + b + " mas el valor " + c + " es igual a: " + d);
+		System.out.println("El resultado de la multibplicacion de estos tres valores es igual a: " +e);
+		System.out.println("El resultado de la division de estos tres valores es igual a: " +f);
+ 
+		// TODO Auto-generated method stub
+
+	}
+
+}
